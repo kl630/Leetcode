a310ffddc0a907e644250c66dd2134d61ca3b4d1
Keren Liu <101140519+kl630@users.noreply.github.com>
2023-03-13 23:11:34 -0400

    Time: 76 ms (76.77%), Space: 46.7 MB (31.94%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.ts
@@ -2,32 +2,25 @@ function evalRPN(tokens: string[]): number {
     let stack: number[] = [];
     for (let i of tokens) {
         if (!isNaN(parseInt(i, 10))) {
-            console.log('current i', i);
             stack.push(parseInt(i, 10));
         } else {
-            let elem1: number = stack[stack.length - 1];
-            let elem2: number = stack[stack.length - 2];
- 
             let temp: number;
             if (i === '+') {
-                temp = elem2 + elem1;
-                console.log('+', temp);
+                temp = stack.pop() + stack.pop(); 
             }
             if (i === '-') {
-                temp = elem2 - elem1;
+                let elem1: number = stack.pop();
+                temp = stack.pop() - elem1;
             }
             if (i === '*') {
-                temp = elem2 * elem1;
+                temp = stack.pop() * stack.pop();  
             }
             if (i === '/') {
-                temp = Math.trunc(elem2 / elem1);
+                let elem1: number = stack.pop();
+                temp = Math.trunc(stack.pop() / elem1);
             }
-            stack.pop();
-            stack.pop();
             stack.push(temp);
         }
     }
-    // now there should be only one element left in the stack, return stack.pop()
-    console.log(stack);
     return stack.pop();
 };
