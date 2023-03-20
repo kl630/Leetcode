2bef3ad726a0f76a7f7b3d6a28b9da70e68d900f
Keren Liu <101140519+kl630@users.noreply.github.com>
2023-03-20 11:57:37 -0400

    Time: 79 ms (57.69%), Space: 46.9 MB (73.08%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    // helper function
+    function recur(node: TreeNode, sum: number) : boolean {
+        if (!node.left && !node.right && sum === 0) return true;
+        if (!node.left && !node.right) return false;
+        if (node.left) {
+            sum -= node.left.val;
+            if (recur(node.left, sum)) return true;
+            sum += node.left.val;
+        }
+        if (node.right) {
+            sum -= node.right.val;
+            if (recur(node.right, sum)) return true;
+            sum += node.right.val;
+        }
+        // can we delete this line? return false;
+        return false;
+    }
+    if (root === null) return false;
+    return recur(root, targetSum - root.val);
+};
