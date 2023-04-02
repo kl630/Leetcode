804f194e76be70a4b7231536ccfe4475cca608ed
Keren Liu <101140519+kl630@users.noreply.github.com>
2023-04-02 19:22:27 -0400

    Time: 95 ms (91.76%), Space: 49.1 MB (56.47%) - LeetHub

---
+++ 0077-combinations/0077-combinations.ts
@@ -0,0 +1,22 @@
+function combine(n: number, k: number): number[][] {
+    // backtracking
+    let result: number[][] = [];
+
+    function backtracking(n: number, k: number, startIndex: number, path: number[]): void {
+        // 终止条件
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+        
+        // 单层搜索 —— for循环横向遍历， 递归纵向遍历
+        for (let i: number = startIndex; i <= n; i++) {
+            path.push(i);
+            backtracking(n, k, i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtracking(n, k, 1, []);
+    return result;
+};
