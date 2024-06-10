6601cea11db17dfcfd8b9428a7f5eb901a72f39e
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-06-10 15:17:59 -0400

    Time: 51 ms (62.99%), Space: 48.9 MB (66.75%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,15 +11,29 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    // inorder: left child -> curNode -> right child
-    const traversal = (curNode, res) => {
-        if (!curNode) return;
-        traversal(curNode.left, res);
-        res.push(curNode.val);
-        traversal(curNode.right, res);
-    };
-    
+    // Recursive solution is trivial, could you do it iteratively?
+    let stack = [];
     let res = [];
-    traversal(root, res);
+
+    // pointer to keep track of which node we are currently visiting
+    let cur = root;
+    // when to terminate the while loop: when the cur node is null && the stack is empty
+    // so the condtion for while loop is (cur node is not null || the stack is not empty)
+    while (cur !== null || stack.length > 0) {
+        // while we are visiting the node, we use stack to store them
+        if (cur !== null) {
+            // this if block achieves the effect of going all the way down to the left
+            stack.push(cur);
+            cur = cur.left;    
+        } else {
+            // here we reach the very left node all the way down
+            // where its left child is null (the cur node, which is null)
+            // we need to re-fetch the not-null node, pop it from the stack and work on it with the res array
+            // re-define the cur node
+            cur = stack.pop();
+            res.push(cur.val);
+            cur = cur.right;
+        }
+    }
     return res;
 };
