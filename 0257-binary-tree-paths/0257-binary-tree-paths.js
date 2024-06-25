0d536628f9896b3e87f7a34b5d48f0274f6805e0
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-06-25 13:18:55 -0400

    Time: 64 ms (27.86%), Space: 51.4 MB (14.32%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    // helper recursion function getPaths(treeNode, path, result)
+    // termination condition: when reaching the leaf node, then push current path
+    // to the result array
+    const getPaths = (treeNode, path, result) => {
+        path.push(treeNode.val);
+        if (treeNode.left === null && treeNode.right === null) {
+            let pathString = path.join("->");
+            result.push(pathString);
+        }
+        if (treeNode.left) {
+            getPaths(treeNode.left, path, result);
+            path.pop();
+        }
+        if (treeNode.right) {
+            getPaths(treeNode.right, path, result);
+            path.pop();
+        }
+    };
+    // traversal order: preorder
+    // middle
+    // left
+    // right
+    
+    // * backtracking * !!!
+    let path = [];
+    let res = [];
+    getPaths(root, path, res);
+    return res;
+};
