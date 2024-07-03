b43fbe2210b6ef373984545e6ba53d3745a25dc1
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-07-02 21:32:54 -0400

    Time: 60 ms (64.88%), Space: 51.3 MB (91.22%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    const traversal = (root, count) => {
+        if (root.left === null && root.right === null && count === 0) return true;
+        if (root.left === null && root.right === null && count !== 0) return false;
+        // why the middle node doesn't matter here??
+        if (root.left) {
+            count -= root.left.val
+            if (traversal(root.left, count)) return true;
+            count += root.left.val;
+        }
+        if (root.right) {
+            count -= root.right.val
+            if (traversal(root.right, count)) return true;
+            count += root.right.val;
+        }
+        return false;
+    };
+    if (root === null) return false;
+    return traversal(root, targetSum - root.val);
+};
