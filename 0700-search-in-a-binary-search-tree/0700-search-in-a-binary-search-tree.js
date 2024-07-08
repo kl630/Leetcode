949f7366b15979311a07408800c7bcad7c824a0d
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-07-08 15:30:59 -0400

    Time: 72 ms (38.01%), Space: 56.5 MB (21.46%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.js
@@ -15,7 +15,7 @@ var searchBST = function(root, val) {
     if (!root) return root;
     if (root.val === val) return root;
     if (root.val < val) {
-        return searchBST(root.right, val); 
+        return searchBST(root.right, val);
     }
     if (root.val > val) {
         return searchBST(root.left, val);
