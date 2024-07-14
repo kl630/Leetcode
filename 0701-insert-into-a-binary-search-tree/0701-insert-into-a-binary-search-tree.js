8bd5573f9cb2664e70999aaaef2537d9e87a5c54
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-07-14 11:36:54 -0400

    Time: 81 ms (97.32%), Space: 58.9 MB (27.01%) - LeetHub

---
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    const newNode = new TreeNode(val, null, null);
+    if (!root) return newNode;
+    if (root.val < val) {
+        root.right = insertIntoBST(root.right, val);
+    } 
+    if (root.val > val) {
+        root.left = insertIntoBST(root.left, val);
+    }
+    return root;
+};
