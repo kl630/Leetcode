12a29e6cffe97bf0b9c325915b94891ed542a036
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-07-20 19:06:55 -0400

    Time: 58 ms (24.38%), Space: 49.3 MB (16.49%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -11,33 +11,14 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    // inorder: left child -> middle -> right child
+    const traversal = (cur, res) => {
+        if (!cur) return;
+        traversal(cur.left, res);
+        res.push(cur.val);
+        traversal(cur.right, res);
+    };
     
-    // TODO: Iteratively
-    let res = [];
-    if (!root) return res;
-    
-    let cur = root;
-    let stack = [];
-    while (stack.length > 0 || cur !== null) {
-        if (cur !== null) {
-            stack.push(cur);
-            cur = cur.left;
-        }  else {
-            // here cur is null, (we went too far)
-            // so we update it properly to the parent node of the initial node (back to previous parent node)
-            cur = stack.pop();
-            res.push(cur.val);
-            if (cur.right) {
-                cur = cur.right;
-            } else {
-                cur = null;
-            }
-            // cur = cur.right;
-            console.log('log res', res);
-        }
-
-    }
-    return res;
-    // maybe the while loop for this one needs two conditions? We'll see
+    let result = [];
+    traversal(root, result);
+    return result;
 };
