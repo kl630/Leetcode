899dcd511caf1b8e276a9a7b46700f5d1d4a8860
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-07-20 15:21:36 -0400

    Time: 45 ms (89.02%), Space: 49.3 MB (17.91%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -11,33 +11,15 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    // let's do it recursively
-    // pre-order: middle -> left -> right
-    // Idea: can we use preorderTraversal itself? instead of defining a new traverse recursion helper function?
-    
-    // Recursive 3 Steps
-    // 1. define recursion function's params and return value
-    // usually params are treeNode and a res array to store node values; and return void
-    const traversal = (root, res) => {
-        if (!root) return;
-        res.push(root.val);
-        if (root.left) {
-            traversal(root.left, res);
-        }
-        if (root.right) {
-            traversal(root.right, res);
-        }
-    };
-    // 2. determine termination condition
-    // when the current node is null, return;
-    
-    // 3. determine the logic at each traversal level
-    // middle: push the middle node's value into the res array
-    // left: call recursion function on the left child
-    // right: call recursion function on the right child
+    // middle -> left -> right
+    const traversal = (cur, res) => {
+        if (!cur) return;
+        res.push(cur.val);
+        traversal(cur.left, res);
+        traversal(cur.right, res);
+    };  
+
     let result = [];
     traversal(root, result);
     return result;
-    
-    // Keren TODO: then let's do it iteratively
 };
