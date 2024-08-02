97f566967a9d6ed2a7e427dff038e5ff1fdd33f9
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-08-02 18:42:05 -0400

    Time: 54 ms (52.98%), Space: 49.4 MB (36.45%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,20 +11,16 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    // pre-order traversal recursively? Give it a try
-    // middle -> left child -> right child
-    // can use invertTree function itself as the recursion function
-    // 1. takes root param, return void
-    // 2. termination condition: when the curNode is null, we return root
-    // 3. logic at each traversal level
-    // middle: swap left child and right child
-    // left: call the invertTree function on the left child
-    // right: call the invertTree function on the right child
+    // preorder traversal
     if (!root) return root;
-    let tempNode = root.left;
+    
+    // middle
+    let temp = root.left;
     root.left = root.right;
-    root.right = tempNode;
-    invertTree(root.left);
-    invertTree(root.right);
+    root.right = temp;
+    // left
+    root.left = invertTree(root.left);
+    // right
+    root.right = invertTree(root.right);
     return root;
 };
