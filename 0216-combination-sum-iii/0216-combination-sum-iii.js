cdc5ad3da0c40cd2693c37fd052a95d8f011482c
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-08-07 15:48:53 -0400

    Time: 49 ms (81.87%), Space: 49.6 MB (34.41%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.js
+++ 0216-combination-sum-iii/0216-combination-sum-iii.js
@@ -4,40 +4,39 @@
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
-    // With Pruning
-    const getSum = (arr) => {
-        let res = 0;
-        for (let i = 0; i < arr.length; i++) {
-            res += arr[i];
-        }
-        return res;
-    };
-    // k is the length of the path []; n is the target sum
     let result = [];
     let path = [];
-    // similar to #77 - combination but with a requirement on the combination sum
     
-    // recursion function: 
-    const backtracking = ( k, startIndex, n) => {
-        // 1. params: k, startIndex; and return value: void
-        // 2. termination condition
-        if (path.length === k && getSum([...path]) === n) {
+    const getSum = (arr) => {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    };
+    
+    const backtracking = (k, n, startInt) => {
+        if ((path.length === k) && getSum(path) === n) {
             result.push([...path]);
             return;
-        }
-        // 3. logic at each recursion level  
-        for (let i = startIndex; i <= 9; i++) {
-            if (getSum([...path]) > n) {
-                return;
-            } else {
-                path.push(i);
-                backtracking(k, i + 1, n);
-                path.pop();    
-            }
+        } 
+        
+        if (path.length > k) return;
+        
+        if (getSum(path) > n) return;
 
+        // k - path.length 
+        // 9 - i + 1 < k - path.length 
+        // i > path.length - k + 10
+        for (let i = startInt; i <= path.length - k + 10; i++) {
+            path.push(i);
+            backtracking(k, n, i + 1);
+            path.pop();
         }
+        
+        return result;
     };
-
-    backtracking(k, 1, n);
+    
+    backtracking(k, n, 1);
     return result;
 };
