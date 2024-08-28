e4af224614005f4399f4783de6099b43cf3fdff2
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-08-28 10:33:29 -0400

    Time: 67 ms (42.98%), Space: 54.2 MB (63.65%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,40 +11,21 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    // height -> postorder traversal
-    // Recursion 3 Steps
-    // 1. recursion function getHeight params: root; return: integer
-    // 2. termination condition: 
-    // if the cur node is null. return 0
-    // 3. logic at each traversal level
-    // left: leftHeight = getHeight(root.left)
-    // need to check if leftHeight === -1, if it is already -1, aka 'imbalanced', then
-    // need to pass it onto the parent node
-    // right: same thing for right and right height
-    // middle: check the height difference between leftHeight and rightHeight. if it is 
-    // larger than 1, it means they are imbalanced, so the height is  -1;
-    // else, it is the Math.max(leftHeight, rightHeight) + 1
-    const getHeight = (treeNode) => {
-        if (treeNode === null) return 0;
-        let leftHeight = getHeight(treeNode.left);
-        if (leftHeight === -1) return -1;
-        let rightHeight = getHeight(treeNode.right);
-        if (rightHeight === -1) return -1;
-        let res;
-        if (Math.abs(leftHeight - rightHeight) > 1) {
-            res = -1;
-        } else {
-            res = Math.max(leftHeight, rightHeight) + 1;
-        }
-        return res;
+    // postorder traversal
+    // if not balanced, return -1
+    const getHeight = (root) => {
+        let result = 0;
+        if (!root) return result;
+        // left
+        let leftHeight = getHeight(root.left);
+        // right
+        let rightHeight = getHeight(root.right);
+        // middle
+        if (leftHeight === -1 || rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        result = Math.max(leftHeight, rightHeight) + 1;
+        return result;
     };
     
-   let result = getHeight(root);
-    if (result === -1) {
-        return false;
-    } else {
-        return true;
-    }
-    
-    
+    return getHeight(root) !== -1;
 };
