8f138cfae0ee8ec18833bb01a07dbed65dfc5638
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-08 19:32:32 -0400

    Time: 56 ms (96.93%), Space: 53.4 MB (49.30%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,30 +3,32 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    // instead of splice method, try using "used" tag
+    // Permutations! Order matters. So no startIndex anymore.
+    // But need to check if the current integer has already been used or not
     let result = [];
     let path = [];
-    let used = Array(nums.length).fill(false);
-    const backtracking = () => {
-            if (path.length === nums.length) {
-                result.push([...path]);
-                return;
-            }
-        
-            for (let i = 0; i < nums.length; i++) {
-                if (used[i]) {
-                    continue;
-                } else {
-                    path.push(nums[i]);
-                    used[i] = true;
-                    backtracking(i); 
-                    path.pop();
-                    used[i] = false;
-                }
-            }
+    let used = new Array(nums.length).fill(false);
 
+    
+    const backtracking = () => {
+        // termination condition
+        if (path.length === nums.length) {
+            result.push([...path]);
+            return;
+        }
+        
+        for (let i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                path.push(nums[i]);
+                used[i] = true;
+                backtracking();
+                path.pop();
+                used[i] = false;
+            }
+        }
+        
     };
+    
     backtracking();
     return result;
-
 };
