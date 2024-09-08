e313747b0e747ca31e44f8aa5a2e81899bfd819a
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-08 10:49:51 -0400

    Time: 85 ms (5.38%), Space: 53.4 MB (82.54%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -1,25 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var subsetsWithDup = function(nums) {
+    // backtracking
+    // tree-level de-duplicate
+    // 1 2 2
     let result = [];
-    let path = [];    
-    result.push([...path]);
+    let path = [];
+    
+    const sortedNums = nums.sort((a, b) => a - b);
 
     const backtracking = (startIndex) => {
-    // no need for termination condition
-        // result.push([...path]);
+        result.push([...path]);
 
-        let sortedNums = nums.sort((a, b) => a - b);
         for (let i = startIndex; i < sortedNums.length; i++) {
+            
             if (i !== startIndex && sortedNums[i] === sortedNums[i - 1]) {
                 continue;
+            } else {
+                path.push(sortedNums[i]);
+                backtracking(i + 1);
+                path.pop();
             }
-            path.push(sortedNums[i]);
-            result.push([...path]);
-
-            backtracking(i + 1);
-            path.pop();
         }
     };
-
+    
     backtracking(0);
     return result;
 };
