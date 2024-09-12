ecf91f2edb19ac034c5be1cbb8dbcd68d030f546
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-12 19:25:19 -0400

    Time: 68 ms (72.02%), Space: 59.5 MB (30.35%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,14 +13,15 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (!root) return null;
     const minVal = Math.min(p.val, q.val);
     const maxVal = Math.max(p.val, q.val);
-    if (root.val < minVal) {
-        return lowestCommonAncestor(root.right, p, q);
+    if (!root) return null;
+    
+    if (root.val >= minVal && root.val <= maxVal) {
+        return root;
     } else if (root.val > maxVal) {
         return lowestCommonAncestor(root.left, p, q);
-    } else if (root.val >= minVal && root.val <= maxVal) {
-        return root;
+    } else if (root.val < minVal) {
+        return lowestCommonAncestor(root.right, p, q);
     }
 };
