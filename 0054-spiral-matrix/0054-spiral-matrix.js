efab50de52d6b8fa2891bbc31680beac75bb9a15
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-16 15:44:16 -0400

    Time: 55 ms (30.98%), Space: 49 MB (16.94%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -1,88 +1,46 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
 var spiralOrder = function(matrix) {
     let result = [];
-    
-    // Handle edge case for empty matrix
-    if (matrix.length === 0) return result;
-    
     let rowStart = 0;
     let rowEnd = matrix.length - 1;
     let colStart = 0;
     let colEnd = matrix[0].length - 1;
     
     while (rowStart <= rowEnd && colStart <= colEnd) {
-        // Traverse right
-        for (let j = colStart; j <= colEnd; j++) {
-            result.push(matrix[rowStart][j]);
+        // left -> right
+        for (let i = colStart; i <= colEnd; i++) {
+            result.push(matrix[rowStart][i]);
         }
-        rowStart++; // Move down
+        rowStart++;
         
-        // Traverse down
+        // top -> down
         for (let i = rowStart; i <= rowEnd; i++) {
             result.push(matrix[i][colEnd]);
         }
-        colEnd--; // Move left
+        colEnd--;
         
-        // Traverse left
+        // right -> left
+        // TODO: check if it is out of bounds
         if (rowStart <= rowEnd) {
-            for (let j = colEnd; j >= colStart; j--) {
-                result.push(matrix[rowEnd][j]);
+            for (let i = colEnd; i >= colStart; i--) {
+                result.push(matrix[rowEnd][i]);
             }
-            rowEnd--; // Move up
+            rowEnd--;    
         }
         
-        // Traverse up
+      
+        // bottom -> up
+        // TODO: check if it is out of bounds
         if (colStart <= colEnd) {
             for (let i = rowEnd; i >= rowStart; i--) {
                 result.push(matrix[i][colStart]);
             }
-            colStart++; // Move right
-        }   
+            colStart++;   
+        }
     }
     
     return result;
 };
-
-// /**
-//  * @param {number[][]} matrix
-//  * @return {number[]}
-//  */
-// var spiralOrder = function(matrix) {
-//     let result = [];
-//     let colStart = 0;
-//     let colEnd = matrix[0].length - 1;
-//     let rowStart = 0;
-//     let rowEnd = matrix.length - 1;
-    
-//     // how to control the traversal? When to stop it?
-//     while (colStart <= colEnd && rowStart <= rowEnd) {
-//         // Traverse right: for loop. TODO: Can I use while loop?
-//         for (let i = colStart; i <= colEnd; i++) {
-//             result.push(matrix[rowStart][i]);
-//         }
-//         rowStart++;
-        
-//         // Traverse down
-//         for (let i = rowStart; i <= rowEnd; i++) {
-//             result.push(matrix[i][colEnd]);
-//         }
-//         colEnd--;
-        
-//         // Traverse left
-//         if (colStart <= colEnd) {
-//             for (let i = colEnd; i >= colStart; i--) {
-//                 result.push(matrix[rowEnd][i]);
-//             }
-//             rowEnd--;
-//         }
-    
-//         // Traverse up
-//         if (rowStart <= rowEnd) {
-//             for (let i = rowEnd; i >= rowStart; i--) {
-//                 result.push(matrix[i][colStart]);
-//             }
-//             colStart++;
-//         }
-//     }
-    
-//     return result;
-// };
