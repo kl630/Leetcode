9099b12c31c569a4166a2061ce2c86e473782102
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-16 18:50:47 -0400

    Time: 67 ms (18.69%), Space: 51.2 MB (91.15%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,26 +11,23 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    // we will calculate height instead because height === depth
-    // when calculating height, we use postorder traversal
+    // post-order traversal: left -> right -> middle
+    // TODO: use a helper function
+//     let result = 0;
+//     if (!root) return result;
     
-    // helper recursion function: getHeight
-    // params: node
-    // return integer(height)
-    // Keren TODO: maybe we can use maxDepth function itself?
-    // const getHeight = () => {
-
-    // };
-    let res = 0;
-    if (!root) return res;
-    // termination condition: node is null, height is 0
-    // left: get the height of left subtree
-    // right:  get the height of right subtree
-    // middle: get the result from left and right => whichever is larger, 
-    // that would be the max depth of the level below that middle node
-    // and the height of that certain middle node will be the result aforementioned + 1
-    let leftHeight = maxDepth(root.left);
-    let rightHeight = maxDepth(root.right);
-    res = Math.max(leftHeight, rightHeight) + 1;
-    return res;
-}
+//     const traverse = (root) => {
+//         if (!root) 
+//     };
+    
+//     traverse(root);
+//     return result;
+    // TODO: try just using maxDepth function itself
+    if (!root) return 0;
+    
+    let leftRes = maxDepth(root.left);
+    let rightRes = maxDepth(root.right);
+    
+    let result = Math.max(leftRes, rightRes) + 1;
+    return result;
+};
