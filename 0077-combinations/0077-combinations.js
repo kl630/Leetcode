9168eb9e938f142b30df62ab4a091ec31d09e60f
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-17 08:38:57 -0400

    Time: 452 ms (75.74%), Space: 121.6 MB (46.36%) - LeetHub

--- 0077-combinations/0077-combinations.js
+++ 0077-combinations/0077-combinations.js
@@ -7,22 +7,22 @@ var combine = function(n, k) {
     let result = [];
     let path = [];
     
-    // Pruning!!
-    // e.g. n = 4, k = 4
-    // startIndex 
-    const backtracking = (n, k, startIndex) => {
+    // TODO: trimming!!
+    
+    const backtracking = (n, k, startInteger) => {
+        // Backtracking 3 Steps
+        // 1. define backtracking helper function: params: n, k, startInteger, and return value: void
+        // 2. termination condition
         if (path.length === k) {
             result.push([...path]);
             return;
         }
-        // n - i + 1 + path.length < k---> need to stop
-        // i > n + path.length - k + 1
-        
-        for (let i = startIndex; i <= n + path.length - k + 1; i++) {
+        // 3. 回溯搜索的遍历过程 logic at each traversal level
+        for (let i = startInteger; i <= n; i++) {
             path.push(i);
-            backtracking(n, k, i + 1);
+            backtracking(n, k, i+1);
             path.pop();
-        } 
+        }
     };
     
     backtracking(n, k, 1);
