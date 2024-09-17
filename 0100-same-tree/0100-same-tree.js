68ef337d716d98baf3588df463551d9650072510
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-17 16:01:00 -0400

    Time: 53 ms (54.62%), Space: 49 MB (69.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,16 +12,13 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (!p && !q) {
-        return true;
-    } else if (!p || !q) {
-        return false;
-    } else if (p && q && p.val !== q.val) {
-        return false;
-    }
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    if (p && q && p.val !== q.val) return false;
     
+    // Otherwise, check one level further
+    // recursively
     let leftRes = isSameTree(p.left, q.left);
     let rightRes = isSameTree(p.right, q.right);
-    result = leftRes && rightRes;
-    return result;
+    return leftRes && rightRes;
 };
