070891615594200eee7de2dd05a28eb3bc59c9d5
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-18 13:27:11 -0400

    Time: 64 ms (81.12%), Space: 53.1 MB (92.06%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.js
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -3,29 +3,31 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    // backtracking
-    // tree-level de-duplicate
-    // 1 2 2
+    // fist need to sort nums array
     let result = [];
     let path = [];
+    let sortedNums = nums.sort((a, b) => a - b);
     
-    const sortedNums = nums.sort((a, b) => a - b);
-
     const backtracking = (startIndex) => {
+        // 1. params:  and return value: void (we update result [])
+        // we need startIndex, so we won't have duplicates [1, 2] and [2, 1]
         result.push([...path]);
-
+        // 2. termination condition: there is no termination condition actually. 
+        // the goal is to collect every node on the tree. the for loop below will naturally end.
+        // 3. logic at each recursion level
+        // e.g. [1, 2, 2, 3]
         for (let i = startIndex; i < sortedNums.length; i++) {
-            
-            if (i !== startIndex && sortedNums[i] === sortedNums[i - 1]) {
+            if (i !== startIndex && sortedNums[i] === sortedNums[i-1]) { // tree-level deduplication
                 continue;
-            } else {
+            } else { // TODO: Do we need else here??
                 path.push(sortedNums[i]);
-                backtracking(i + 1);
+                backtracking(i+1);
                 path.pop();
             }
-        }
+            
+        }   
     };
-    
+ 
     backtracking(0);
     return result;
 };
