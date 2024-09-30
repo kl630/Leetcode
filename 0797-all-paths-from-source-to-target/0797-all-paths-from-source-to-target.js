d22e667f726a2eaff8ebd9df081a640feb4fe05e
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-09-30 07:57:41 -0400

    Time: 201 ms (5.42%), Space: 59.8 MB (23.15%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.js
@@ -3,27 +3,35 @@
  * @return {number[][]}
  */
 var allPathsSourceTarget = function(graph) {
-    // DFS - Depth First Search
-    // 1.  define recursion function and params
-    // 2. decide on when to terminate the recursion
-    // 3. traverse
-    let res = [];
+    // the given input is an ajacency list representation of a graph
+    let result = [];
     let path = [];
-
-    function dfs(graph, start) {
-        if (start === graph.length - 1) { // base case: when start node is the last node
-            res.push([...path]);
+    
+    // [[1,2],[3],[3],[]]
+    // number of nodes = graph.length; 
+    // e.g. 4 nodes; node 0, node 1, node 2, node 3
+    const dfs = (graph, start) => {
+        // TODO: how to handle the first node- node 0?
+        
+        // 1. dfs params: graph,  and return value: void, just update result 
+        // 2. base case
+        // when we reach the last node (node n-1), then we will collect the current path and push it to result []
+        if (start === graph.length - 1) {
+            result.push([...path]);
             return;
         }
-        for (let i = 0; i < graph[start].length; i++) {
-            path.push(graph[start][i]);
-            dfs(graph, graph[start][i]);
-            path.pop(); // the neighbor node is removed from the path to backtrack and 
-                        // explore other paths
+        // 3. how to process the path starting from the current node
+        // for (let i = start; i < graph.length; i++) {
+        let neighbors = graph[start];
+        for (let neighbor of neighbors) { // let.(index??)..in ... OR let.(element??)..of ...??
+            console.log('log neighbor', neighbor);
+            path.push(neighbor);
+            dfs(graph, neighbor);
+            path.pop();
         }
-    }
-
+        //}
+    };
     path.push(0);
     dfs(graph, 0);
-    return res;
+    return result;
 };
