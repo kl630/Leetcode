c2f4269146bc84e6f81c69dd1740a6730219a2ee
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-03 10:08:17 -0400

    Time: 60 ms (90.06%), Space: 54.7 MB (95.03%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.js
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.js
@@ -11,37 +11,44 @@
  * @return {number[]}
  */
 var findMode = function(root) {
-    let pre = null;
-    let count = 0;
-    let maxCount = 0;
     let result = [];
+    let pre = null;
+    let maxCount = -Infinity;
+    let count;
+
     
-    const traversal = (cur) => {
-        if(!cur) return;
-        // left:
-        traversal(cur.left);
-        // middle:
-        if (!pre) {
+    const traverse = (cur) => {
+
+        if (!cur) return;
+        // left
+        traverse(cur.left);
+        
+        // middle
+ 
+        if (pre === null) {
             count = 1;
         } else if (pre.val === cur.val) {
-            count ++;
+            count++; 
         } else if (pre.val !== cur.val) {
-            count = 1;
+            count = 1;     
         }
         
+        // Todo: > or >=??
         if (count === maxCount) {
-            result.push(cur.val);  
-        }
+            // maxCount = Math.max(maxCount, count);
+            result.push(cur.val);
+        } 
         if (count > maxCount) {
-            maxCount = count;
+            // maxCount = Math.max(maxCount, count);
+            // maxCount = count;
             result = [];
             result.push(cur.val);
         }
         pre = cur;
-        // right:
-        traversal(cur.right);
+        maxCount = Math.max(maxCount, count);
+        // right
+        traverse(cur.right);
     };
-    
-    traversal(root);
+    traverse(root);
     return result;
 };
