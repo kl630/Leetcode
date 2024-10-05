4eac6f49c5d1052af97aae5ae604be05c7906aba
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-05 09:08:50 -0400

    Time: 56 ms (73.05%), Space: 53 MB (41.32%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
@@ -11,78 +11,30 @@
  * @return {number}
  */
 var maxAncestorDiff = function(root) {
-    // let maxDiff = -Infinity;
-    // middle -> left -> right
-
+    // TODO: where should i put maxDiff? Global or in bfs function?
+    let maxDiff = - Infinity;
     
-    // helper function: dfs()
-    //   3
-    // 1   6
-    //    4  7
-
-    // max = 8, min = 8
-    // maxDiff = -Infinity, 
-    // node = 3
-    // dfs(1)
-    const dfs = (node, curMin, curMax) => {
-    // 1. Base case: if the node is null, return the difference between the current max and min values
-    if (!node) return curMax - curMin;
+    const bfs = (node, curMin, curMax) => {
+        if (!node) return;
+        
+        if (node.val > curMax) {
+            curMax = node.val;
+        }
+        
+        if (node.val < curMin) {
+            curMin = node.val
+        }
+        
+        // go to left side
+        bfs(node.left, curMin, curMax);
+        // go to right side
+        bfs(node.right, curMin, curMax);
+        
+        // TODO Solution 2: return maxDiff
+        maxDiff = Math.max(maxDiff, curMax - curMin);
+    };
     
-    // 2. Update the current min and max values with the node's value
-    curMin = Math.min(curMin, node.val);
-    curMax = Math.max(curMax, node.val);
-
-    // 3. Recurse into the left and right subtrees
-    let leftMaxDiff = dfs(node.left, curMin, curMax);
-    let rightMaxDiff = dfs(node.right, curMin, curMax);
-
-    // 4. Return the maximum difference found in either the left or right subtree
-    return Math.max(leftMaxDiff, rightMaxDiff);
+    // TODO Solution 2: return bfs(root, root.val, root.val);
+    bfs(root, root.val, root.val);
+    return maxDiff;
 };
-//     const dfs = (node, curMin, curMax) => {
-
-//         // let min = root.val;
-//         // let max = root.val;
-//         // 1.TODO: params: node, and return value: maxDiff
-//         // 2. TODO: base case
-//         if (!node) return curMax - curMin;
-        
-//         // 3. TODO: logic at each recursion level
-//         // traversal order doesn't really matter. I use inorder here.
-//         // the goal is to traverse the entire tree and update max and min 
-//         // left:
-//         dfs(node.left, curMin, curMax);
-//         // middle:
-//         console.log('log node.val', node.val);
-//         if (node.val > curMax) {
-//             curMax = node.val;
-//         }
-        
-//         if (node.val < curMin) {
-//             curMin = node.val;
-//         }
-//         console.log('log min', curMin);
-        
-//         console.log('log max', curMax);
-//         maxDiff = Math.max(maxDiff, curMax - curMin);
-//         console.log('log maxDiff', maxDiff);
-
-//         // right:
-//         dfs(node.right, curMin, curMax);
-//         // maxDiff = Math.max(maxDiff, curMax - curMin);
-
-    
-
-//         return maxDiff;
-//     };
-    
-    let leftRes = dfs(root.left, root.val, root.val);
-    console.log('log leftRes', leftRes);
-    let rightRes = dfs(root.right, root.val, root.val);
-    console.log('log rightRes', rightRes);
-
-    return Math.max(leftRes, rightRes);
-};
-
-
-// use an example
