e282bfeeba6a340618c3008d3ab2ac72b0847f12
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-07 13:23:44 -0400

    Time: 1100 ms (45.10%), Space: 199 MB (11.77%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -6,40 +6,59 @@
  * @return {boolean}
  */
 var validPath = function(n, edges, source, destination) {
-    if (source === destination) return true; // if source and destination are same, return true
-
-    // Build adjacency list from edges
-    const adjacencyList = new Map();
-    for (let [u, v] of edges) {
-        if (!adjacencyList.has(u)) {
-            adjacencyList.set(u, []);
+    // TODO: convert edges [[]] into an adjacency list
+    // TODO: optimize: node values are 0-indexed, no need for key
+    if (edges.length === 0) return true;
+    let adjList = new Map();
+    // [[0,1],[1,2]  ** bi-directional
+        // => { '0' : [1, 2], '1': [0, 2], '2': [0] }
+    for ([u, v] of edges) {
+        if (!adjList.has(u)) {
+            adjList.set(u, []);
         }
-        adjacencyList.get(u).push(v);
-
-        if (!adjacencyList.has(v)) {
-            adjacencyList.set(v, []);
+        adjList.get(u).push(v);
+        
+        if (!adjList.has(v)) {
+            adjList.set(v, []);
         }
-        adjacencyList.get(v).push(u);
+        adjList.get(v).push(u);
     }
-
-    // DFS with visited Set to prevent revisiting nodes
+    
+    console.log('log adjList', adjList);
     let visited = new Set();
     
-    const dfs = (start) => {
-        if (start === destination) return true;
-        visited.add(start);
-
-        const neighbors = adjacencyList.get(start);
+    // bfs
+    const dfs = (src, dest) => {
+        // return boolean val
+        // TODO: base case
+        // detected a cycle!
+        if (visited.has(src)) {
+            return false;
+        }
         
+        visited.add(src);
+        let neighbors = adjList.get(src);
+        // e.g. [1, 2]
+        
+        // if found a path between source to destination
+            // return true
         for (let neighbor of neighbors) {
-            if (!visited.has(neighbor)) {
-                if (dfs(neighbor)) {
-                    return true; // early return if we find the destination
+            if (neighbor === dest) {
+                return true;
+            } else {
+            // not found yet, call dfs on neighboring nodes
+                // TODO!!!
+                // return dfs(neighbor, dest); // dfs has a return value
+                if (dfs(neighbor, dest)) {
+                    return true;
                 }
             }
         }
-        return false; // no path found from this node
+      
+        return false;
     };
+    
 
-    return dfs(source);
+    
+    return dfs(source, destination);
 };
