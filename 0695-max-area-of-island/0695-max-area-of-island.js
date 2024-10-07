0c668df75d6b25cb609040aa2233b5691e2e1233
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-07 14:35:31 -0400

    Time: 179 ms (5.04%), Space: 57.9 MB (6.47%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -4,38 +4,41 @@
  */
 var maxAreaOfIsland = function(grid) {
     let maxArea = 0;
-    const m = grid.length;
-    const n = grid[0].length;
-    
+    // bfs (x, y) 
+    // params: cur location, and return value: curArea
+    // just to update result
     const dfs = (x, y) => {
-        // 1. params and return value: return area val
-        // 2. termination condition
-        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] !== 1) {
+        let curArea = 0;
+
+        // TODO: base case:
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] !== 1) {
             return 0;
         }
-        // 3. handling the path starting from the current position
-        // TODO: STILL A BIT CONFUSED ON HOW TO HANDLE THE FIRST FOUND 1
-        // set area initially as 1, so it includes the current found 1
-        let area = 1;
-        // Do we still need this check: grid[x][y] === 1? No, it was already checked
-        // try to expand the island by checking up/down/left/right
-        // But need to de-duplicate, mark the current grid[x][y] as 0, so it won't visited more than once
-        grid[x][y] = '#';
-        area += dfs(x-1, y);
-        area += dfs(x+1, y);
-        area += dfs(x, y-1);
-        area += dfs(x, y+1);
+        // recursive logic
+        // when we are here, we are currently at a '1'
+        curArea++;
+        console.log('log curArea', curArea);
+        // mark cur position as visited
+        grid[x][y] = '#'; // TODO: Or to change it to '0', OR use a helper
+                          // visited grid if we cannot modify original grid
 
-        return area;
+        // Explore its 4 neighboring cells- invoke bfs on 4 neighbors
+        curArea += dfs(x-1, y);
+        curArea += dfs(x+1, y);
+        curArea += dfs(x, y-1);
+        curArea += dfs(x, y+1);
+        return curArea;
     };
     
-    // loop throught grid and loop for 1
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1) {
-                maxArea = Math.max(maxArea, dfs(i, j));
+    // Loop through the grid
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === 1) { // do operations on '1'
+                // invoke bfs on it
+                // get curArea and compare it with maxArea
+                maxArea = Math.max(dfs(i, j), maxArea);
             }
         }
     }
-    return maxArea;
+    return maxArea; // an integer
 };
