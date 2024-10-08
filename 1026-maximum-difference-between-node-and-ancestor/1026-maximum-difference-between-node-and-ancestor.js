b0b66e175b8f79d8e0aec6fb85594c2de9c0950a
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-07 20:18:34 -0400

    Time: 66 ms (25.45%), Space: 52.8 MB (56.97%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.js
@@ -11,29 +11,34 @@
  * @return {number}
  */
 var maxAncestorDiff = function(root) {
-    // TODO: where should i put maxDiff? Global or in bfs function?
-    
-    const bfs = (node, curMin, curMax) => {
-        if (!node) return curMax - curMin;
+    let maxDiff = -Infinity;
+
+    const traverse = (node, curMin, curMax) => {
+        // Keren TODO: when to define it in helper recursion function?
+        // vs. when to define it as global variable
+        // 1. params: TODO; and return value: void, update maxDiff
+
+        // also need a curMin and curMax
+        // 2. base case
+        if (!node) return;
+        // 3. recursive logic
+        // preorder
+        // middle
+        curMin = Math.min(curMin, node.val);
+        curMax = Math.max(curMax, node.val);
+        // e.g. root is 8, now we are here, curMin = 8, curMax = 8
+        // left
+        traverse(node.left, curMin, curMax); // here, curMin and curMax got reset again if curMin and curMax are defined inside helper function
+        // when curMin and curMax are defined as global variables
+        // still has reference to pre curMin and curMax
+        // curMin = 3, curMax = 8,
         
-        if (node.val > curMax) {
-            curMax = node.val;
-        }
-        
-        if (node.val < curMin) {
-            curMin = node.val
-        }
-        
-        // go to left side
-        let leftRes = bfs(node.left, curMin, curMax);
-        // go to right side
-        let rightRes = bfs(node.right, curMin, curMax);
-        
-        // TODO Solution 2: return maxDiff
-       return Math.max(leftRes, rightRes);
+        // Keren TODO: minDiff in BST?? vs. Mode in BST
+        // right
+        traverse(node.right, curMin, curMax); // this way, when we start traversing to root.right, we will always compare to the root node
+        maxDiff = Math.max(maxDiff, curMax - curMin);
     };
     
-    // TODO Solution 2: return bfs(root, root.val, root.val);
-     return  bfs(root, root.val, root.val);
-  
+    traverse(root, root.val, root.val);
+    return maxDiff;
 };
