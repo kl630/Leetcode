1d424f1cde22a4224d88f2b35e3a48c17a250291
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-09 14:07:38 -0400

    Time: 248 ms (46.49%), Space: 48.8 MB (78.95%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.js
@@ -7,19 +7,18 @@ var getMaximumGold = function(grid) {
     let visited = Array.from({length: grid.length}, () => new Array(grid[0].length).fill(false));
     
     // backtracking: take a position, returns the max gold that be collected from that position
-    const backtracking = (x, y, curMax) => {
+    const backtracking = (x, y) => {
         let result = 0;
-        // TODO: base case
+        // base case
         if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] === 0 || visited[x][y]) {
-            return curMax;
+            return 0;
         }
         // e.g. we are at 6
         visited[x][y] = true;
-        curMax += grid[x][y];
-        result = Math.max(backtracking(x-1, y, curMax), backtracking(x+1, y, curMax), backtracking(x, y-1, curMax), backtracking(x, y+1, curMax))
+        // curMax += grid[x][y];
+        result = Math.max(backtracking(x-1, y), backtracking(x+1, y), backtracking(x, y-1), backtracking(x, y+1)) + grid[x][y];
         
         visited[x][y] = false; // backtracking
-        curMax -= grid[x][y];
         return result;
     };
     
@@ -29,7 +28,7 @@ var getMaximumGold = function(grid) {
             // invoke backtracking on non-zero positions
             if (grid[i][j] !== 0) {
                 // compare max with the result of backtracking() and update max (global variable)
-                max = Math.max(max, backtracking(i, j, 0));
+                max = Math.max(max, backtracking(i, j));
             }
         }
     }
