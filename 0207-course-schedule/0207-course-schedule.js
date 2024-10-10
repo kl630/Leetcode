3a82b62d09d5c24b2cd6444bbc7b4ff13ea7d675
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-10 11:51:48 -0400

    Time: 61 ms (89.86%), Space: 56 MB (49.69%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,47 +4,51 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    // build an adjacency list
-    // detect if there is a cycle in the graph
+    // Detect if there a cycle in the graph
+    // e.g. [ [1], [0] ]. 0 => 1 , 1 => 0 ---- a cycle
+    // TODO: build an adjacency list
     let adjList = Array.from({ length: numCourses }, () => []);
-    // TODO: 
     for (let [end, start] of prerequisites) {
-        adjList[start].push(end); 
+        adjList[start].push(end);
     }
     
     let visited = new Array(numCourses).fill(false);
     let visiting = new Array(numCourses).fill(false);
-
-    const dfs = (node) => { // node.val is also index's val
-        // 1. params: node, return value: boolean
-        // 2. base case
-        if (visiting[node]) return false; // end up in a cycle
-        if (visited[node]) return true; // has thoroughly and successfully processed that node
-        // 3. recursive logic
-        // if found a cycle -> return false
-        // if not found a cycle yet, keep recursion
-        visiting[node] = true;
-
-        const neighbors = adjList[node];
-        for (let neighbor of neighbors) {
-            if (!dfs(neighbor)) {
-                return false;
-            }
-        }
-        // Done visiting. Reset it to visiting = false
-        visiting[node] = false;
-        // Mark it has visited
-        visited[node] = true;
+    
+    
+    const dfs = (node) => {
+        // return: boolean
+        // How to detect a cycle?
+        // whenever we visit a node, we mark it as visiting
+        // then we visit its neighbors -- invoke dfs() on neighbors
+        // when we visit the neighbor, we check its neighbors
+        if (visiting[node]) return false;
+        // if we found a cycle
+            // return false;
+        if (visited[node]) return true; // we visit a fully processed node. so need to process it again. Return true;
         
+        visiting[node] = true;
+        for (let neighbor of adjList[node]) {
+            // invoke dfs on each neighbor
+            // if the result of dfs(neighbor) is false
+            if (!dfs(neighbor)) return false;
+                // return false
+        }
+    
+        visiting[node] = false;
+        visited[node] = true;
         return true;
     };
     
-    // loop through all courses and invoke bfs on each course/node
+    // loop through all nodes/ courses, 
     for (let i = 0; i < numCourses; i++) {
-        // if the result of bfs is false
-        if (!visited[i] && !dfs(i)) {
-            // return false
-            return false;
+        // invoke dfs() on each node
+        // if the result of dfs() is false
+        if (!visited[i]) {
+            if (!dfs(i)) {
+                // return false;
+                return false;
+            }   
         }
     }
     return true;
