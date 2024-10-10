40cc53615cd003e7bf8765e6f1cc673250a9ec42
Keren Liu <101140519+kl630@users.noreply.github.com>
2024-10-10 10:15:06 -0400

    Time: 73 ms (76.13%), Space: 68.3 MB (39.73%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -11,22 +11,58 @@
  * @return {number}
  */
 var countNodes = function(root) {
-    // postorder traversal
-    // consider it has a normal tree first
-    // Recursion 3 Steps
-    // 1. recursion function's params and return value
-    // param: node and return an integer (number of tree nodes)
-    // 2. termination condition
-    // if the current node is null, return 0;
-    if (!root) return 0;
-    // 3. logic at each traversal level
-    // left child: 
-    let left = countNodes(root.left);
-    // right child:
-    let right = countNodes(root.right);
-    // middle:
-    let res = left + right + 1;
-    return res;
+    let result = 0;
+    if (!root) return result;
+        
+    // how to check if a complete tree is a FULL tree?
+    // leftmost node's depth === rightmost node's depth
+        
+    const getLeftDepth = (root) => {
+        // return: the depth of the deepest leftmost node to the root;
+        if (!root) return 0;
+        
+        let depth = 1;
+        while (root.left) {
+            root = root.left
+            depth++;
+        }
+        return depth;
+    };
     
-    // then leverage complete tree's special properties
+    const getRightDepth= (root) => {
+         // return: the depth of the deepest rightmost node to the root;
+        if (!root) return 0;
+        
+        let depth = 1;
+        while (root.right) {
+            root = root.right
+            depth++;
+        }
+        return depth;
+    };
+    
+    if (getLeftDepth(root) === getRightDepth(root)) {
+        result = Math.pow(2, getLeftDepth(root)) - 1;
+    } else {
+        let left = countNodes(root.left);
+        let right = countNodes(root.right);
+        result = left + right + 1;
+    }
+    // If the the complete tree is a FULL tree,
+        // then great! number of tree nodes = 2 ^ depth - 1
+    
+    // if it is not full: total num = left num + right num + 1
+        // then examine its left subtree
+        // If the the complete tree is a FULL tree,
+            // then great! number of tree nodes = 2 ^ depth - 1
+        // If it's not full, examine its left subtree and right subtree....
+        
+    // and right subtree
+       // If the the complete tree is a FULL tree,
+            // then great! number of tree nodes = 2 ^ depth - 1
+        // If it's not full, examine its left subtree and right subtree....
+   // Base case: we are at a leaf node --- total sum = left num + right num + 1; and here left num and right num are both 0
+    // So, base case is the cur node is null, total sum = 0
+
+    return result;
 };
